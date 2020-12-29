61720ce3422da5610d898741a50415b085c212a3
AI Team <dtkhangbk@gmail.com>
2020-12-29 12:52:19 +0700

    Add depen file

---
+++ build/trt_l2norm_helper/CMakeFiles/trt_l2norm_helper.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ai/ai/mtcnn_facenet_cpp_tensorRT/trt_l2norm_helper/l2norm_helper.cpp" "/home/ai/ai/mtcnn_facenet_cpp_tensorRT/build/trt_l2norm_helper/CMakeFiles/trt_l2norm_helper.dir/l2norm_helper.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/ai/ai/NGT-1.12.2/samples/l2-uint8/lib"
+  "../trt_l2norm_helper"
+  "/usr/include/aarch64-linux-gnu"
+  "/usr/local/cuda/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
